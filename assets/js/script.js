2406198df5f6c30a108028a58fc123d4d279d50c
raedaltaki <raed.simon@gmail.com>
2020-12-31 23:02:59 -0500

    add save and load tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,73 @@
+var tasks = {};
+var loadTasks = function()
+{
+    tasks = localStorage.getItem("tasks");
+    tasks = JSON.parse(tasks);
+    if (!tasks) 
+    {
+        tasks =
+        {
+            time9AM: "",
+            time10AM: "",
+            time11AM: "",
+            time12PM: "",
+            time1PM: "",
+            time2PM: "",
+            time3PM: "",
+            time4PM: "",
+            time5PM: ""
+        };
+    }
+    else
+    {
+        $("li").each(function() 
+        {
+            var index = "time"+ $(this).find(".hour").text().trim();
+            $(this).find(".description").val(tasks[index]);
+        });
+    }
+    checkTime();
+}
+
+
 var checkTime = function()
 {
     var currentHour = "12PM";// moment().format("hA");
-    // currentHour= parseInt(currentHour);
     $("li").each(function() 
     {
         var list = $(this).find(".hour").text().trim();
         var listTime = moment(list,"hA");
-
         currentHour = moment(currentHour,"hA");
-        console.log(listTime);
-        console.log(currentHour);
+ 
         if (listTime.isBefore(currentHour))
         {
             $(this).find(".description").addClass("past");
-            console.log("Before");
         }else 
         if (listTime.isAfter(currentHour))
         {
             $(this).find(".description").addClass("future");
-            console.log("After");
         } else 
         if (listTime.isSame(currentHour))
         {
             $(this).find(".description").addClass("present");
-            console.log("same");
         }
-        
     });
-
-    //$("ul").children().each( console.log($(this)));
-
-    // if($(".hour").text().trim() === currentHour)
-    // {
-    //     $(".hour").text("RAED");
-    // };
 }
+
+// $(".description").on("change", function()
+// {
+//     var index = "time"+ $(this).closest(".row").find(".hour").text().trim();
+//     tasks[index] = $(this).val();
+// });
+
+$(".saveBtn").on("click", function()
+{
+    var index = "time"+ $(this).closest(".row").find(".hour").text().trim();
+    tasks[index] = $(this).closest(".row").find(".description").val();
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+});
 
 var today = moment().format("dddd, MMMM Do");
 $("#currentDay").text(today);
-checkTime();
-
+loadTasks();
 setInterval(checkTime,(60*1000));
